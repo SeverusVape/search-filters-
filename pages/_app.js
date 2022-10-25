9cdb2df79e0adfba40544a431768c75a53b4cc86
Veter v Golove <koshelevworkmail@gmail.com>
2022-10-25 19:32:19 -0400

    Property description page

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,30 @@ import Router from "next/router";
 import Head from "next/head";
 import NProgress from "nprogress";
 import { ChakraProvider } from "@chakra-ui/react";
+
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
+    NProgress.configure({ showSpinner: false });
+
+    Router.events.on("routeChangeStart", () => {
+        NProgress.start();
+    });
+
+    Router.events.on("routeChangeComplete", () => {
+        NProgress.done();
+    });
+
     return (
         <>
-            <Head></Head>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    // href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+                    crossOrigin="anonymous"
+                    referrerPolicy="no-referrer"
+                />
+            </Head>
             <ChakraProvider>
                 <Layout>
                     <Component {...pageProps} />
